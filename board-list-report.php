b18ecff16b91bdd0323ab2069800ddfa87417f17
batmaster <batmaster_kn@hotmail.com>
2015-11-18 14:05:56 +0700

    board-list-report.php

---
+++ board-list-report.php
@@ -0,0 +1,20 @@
+<h2 class="sub-header">รายการบอร์ด</h2>
+<div class="panel panel-default">
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>ยี่ห้อ</th>
+                <th>ชื่ออุปกรณ์</th>
+                <th>ประเภท</th>
+                <th>S/N</th>
+                <th>ส่งไปที่</th>
+                <th>หมายเหตุ</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody id="table-body-model-instock">
+
+        </tbody>
+    </table>
+</div>
